8977787b2312e186e08cd159bf80a4c3217daa21
Atlinx <atlinuxtm@gmail.com>
2022-08-17 20:47:08 -0400

    Refurbish scene

---
+++ Scripts/LinkButton.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+
+public partial class LinkButton : Button
+{
+    [Export]
+    public string Link { get; set; }
+
+    public override void _Ready()
+    {
+        Connect("pressed", this, nameof(OnPressed));
+    }
+
+    private void OnPressed()
+    {
+        OS.ShellOpen(Link);
+    }
+}
---
+++ Scripts/VisitExhibitUIButton.cs
@@ -0,0 +1,21 @@
+﻿using Godot;
+using GodotOnReady.Attributes;
+
+public partial class VisitExhibitUIButton : Button
+{
+    [OnReadyGet]
+    private CameraController cameraController;
+    [OnReadyGet]
+    private Exhibit exhibit;
+
+    [OnReady]
+    public void RealReady()
+    {
+        Connect("pressed", this, nameof(OnPressed));
+    }
+
+    private void OnPressed()
+    {
+        cameraController.Visit(exhibit.Position);
+    }
+}
